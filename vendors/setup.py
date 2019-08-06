e99478dfb3cb61a17d8b17ad9a72b2d97b849e99
Laoska <lbgm2011@gmail.com>
2019-08-06 12:39:36 +0100

    update versions

--- vendors/setup.py
+++ vendors/setup.py
@@ -25,7 +25,7 @@ class InstallCommand(install):
 
 setup(
     name='envoxyd',
-    version='0.0.8',
+    version='0.0.9',
     description='Envoxyd',
     author='Matheus Santos',
     author_email='vorj.dux@gmail.com',
