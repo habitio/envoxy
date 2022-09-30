477533bf83cc3f17453ab2bc345e0c54f83f95d4
Matheus Santos <vorj.dux@gmail.com>
2022-09-30 19:19:41 +0100

    Updating to UWSGI==2.0.20

--- vendors/setup.py
+++ vendors/setup.py
@@ -21,14 +21,14 @@ class InstallCommand(install):
 
 setup(
     name='envoxyd',
-    version='0.0.32',
+    version='0.1.0',
     description='Envoxyd',
     author='Matheus Santos',
     author_email='vorj.dux@gmail.com',
     url='https://github.com/muzzley/envoxy',
     packages=find_packages(exclude=["uwsgi", "templates", "tests"]),
     install_requires=[
-        "envoxy>=0.1.38",
+        "envoxy>=0.2.0",
         "flask_cors==3.0.9",
         "isort>=4.2.5,<5"
     ],
