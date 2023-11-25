c2f98fdadba62c33b7b1506dd1e94df0fc61d136
Matheus Santos <vorj.dux@gmail.com>
2023-11-25 17:39:25 +0000

    version bump

--- vendors/setup.py
+++ vendors/setup.py
@@ -21,14 +21,14 @@ class InstallCommand(install):
 
 setup(
     name='envoxyd',
-    version='0.1.9',
+    version='0.1.10',
     description='Envoxyd',
     author='Matheus Santos',
     author_email='vorj.dux@gmail.com',
     url='https://github.com/muzzley/envoxy',
     packages=find_packages(exclude=["uwsgi", "templates", "tests"]),
     install_requires=[
-        "envoxy>=0.2.9",
+        "envoxy>=0.2.10",
         "flask_cors==3.0.9",
         "isort>=4.2.5,<5"
     ],
