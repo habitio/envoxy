045a9bc2496c5191bd47356f6c29a6e0260d1f5b
Laoska B <lbgm2011@gmail.com>
2024-11-28 17:48:09 +0000

    upgrade dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM envoxy-ubuntu:20.04
+FROM envoxy-ubuntu:20.04-311
 
 RUN useradd -ms /bin/bash envoxy
 
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open(find_file("README.md"), encoding="utf-8") as f:
 
 setup(
     name="envoxy",
-    version="0.2.15",
+    version="0.2.17",
     description="Envoxy Platform Framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
