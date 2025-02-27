0d2fced8482fe29742ace45e9784a85b9b29797f
Laoska B. <lbgm2011@gmail.com>
2025-02-27 17:17:18 +0000

    envoxy==0.3.1; envoxyd==0.2.1

--- vendors/setup.py
+++ vendors/setup.py
@@ -24,7 +24,7 @@ class InstallCommand(install):
 
 setup(
     name="envoxyd",
-    version="0.2.0",
+    version="0.2.1",
     description="Envoxyd",
     author="Matheus Santos",
     author_email="vorj.dux@gmail.com",
