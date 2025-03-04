12436a29374235b112a2eac9c899630ca786e696
Laoska B. <lbgm2011@gmail.com>
2025-03-04 17:34:34 +0000

    celerybeat

--- src/envoxy/celery/client.py
+++ src/envoxy/celery/client.py
@@ -24,6 +24,33 @@ class Client:
 
         app = Celery('envoxy', broker=broker)
 
+        # celerybeat config
+
+        if Config.get("mongodb_servers") and "celery" in Config.get("mongodb_servers"):
+
+            _mongodb_celery = Config.get("mongodb_servers").get("celery", {})
+
+            _mongodb_url = f"mongodb://{_mongodb_celery['user']}:{_mongodb_celery['passwd']}@{_mongodb_celery['host']}:{_mongodb_celery['port']}"
+
+            config = {
+                "mongodb_scheduler_db": _mongodb_celery.get("db"),
+                "mongodb_scheduler_url": _mongodb_url
+            }
+
+            app.conf.update(**config)
+
+        # task result backend config
+
+        if Config.get("redis_servers") and "celery" in Config.get("redis_servers"):
+
+            _redis_celery = Config.get("redis_servers").get("celery", {})
+
+            config = {
+                "result_backend": f"redis://{_redis_celery['bind']}/{_redis_celery['db']}"
+            }
+
+            app.conf.update(**config)
+
         app.autodiscover_tasks()
 
         Client.app = app
