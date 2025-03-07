e7b798ec6082bb2dd5dc27c8031970043198afc7
Laoska B. <lbgm2011@gmail.com>
2025-03-07 16:17:36 +0000

    route tasks to config queue

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open(find_file("README.md"), encoding="utf-8") as f:
 
 setup(
     name="envoxy",
-    version="0.3.3",
+    version="0.3.6",
     description="Envoxy Platform Framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
